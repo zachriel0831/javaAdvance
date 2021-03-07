d59f0c768597e3d56f97ffb56e547ebebf43c6ca
zachriel0831 <tc101fubonzack@gmail.com>
2021-03-07 14:23:59 +0800

    first commit

---
+++ src/main/java/com/ntu/shoppingcart/models/common/BaseResponseModel.java
@@ -0,0 +1,9 @@
+package com.ntu.shoppingcart.models.common;
+
+import lombok.Data;
+
+@Data
+public class BaseResponseModel {
+	private Users users;
+
+}
---
+++ src/main/java/com/ntu/shoppingcart/models/common/Products.java
@@ -0,0 +1,5 @@
+package com.ntu.shoppingcart.models.common;
+
+public class Products {
+
+}
